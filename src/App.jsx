2729d9d22d50fea9f0f9fa3998fd222b92d9f195
lam4est <lam058b@gmail.com>
2025-11-08 23:56:22 +0700

    Building admin interface: dashboard and sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ import Favorite from "./pages/Favorite.jsx";
 import Footer from "./components/Footer.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
+import Layout from "./pages/admin/Layout.jsx";
+import ListShows from "./pages/admin/ListShows.jsx";
+import ListBookings from "./pages/admin/ListBookings.jsx";
+import Dashboard from "./pages/admin/Dashboard.jsx";
+import AddShows from "./pages/admin/AddShows.jsx";
 
 const App = () => {
 
@@ -26,6 +30,12 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/favorite" element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path="add-shows" element={<AddShows/>}/>
+            <Route path="list-shows" element={<ListShows/>}/>
+            <Route path="list-bookings" element={<ListBookings/>}/>
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </div>
